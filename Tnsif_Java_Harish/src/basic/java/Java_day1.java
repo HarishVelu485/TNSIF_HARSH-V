4167a9edff601d2f09f6aa7cc3132d7f71e37655
Harish.V <Harish.V@LAPTOP-411LL65A>
2025-09-24 23:10:46 +0530

    completed

---
+++ Tnsif_Java_Harish/src/basic/java/Java_day1.java
@@ -0,0 +1,11 @@
+package basic.java;
+
+public class Java_day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+       
+	}
+
+}
