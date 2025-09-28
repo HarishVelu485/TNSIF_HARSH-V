c2b260a930165107173bd27492f2d5af8eec4707
Harish.V <Harish.V@LAPTOP-411LL65A>
2025-09-28 21:52:12 +0530

    commit

---
+++ Tnsif_Java_Harish/src/basic/java/Addtion.java
@@ -0,0 +1,16 @@
+package basic.java;
+import java.util.*;
+
+public class Addtion {
+	public static void main(String[] arys) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the number1:");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" in "+c);
+		
+			
+	}
+
+}
